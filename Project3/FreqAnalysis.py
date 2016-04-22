0ccb555c435829db76c6c08270790422b44aa87e
Chanel Aquino <chanelcodes@gmail.com>
2016-04-21 23:42:08 -0700

    gui stuff for pyplot

--- Project3/FreqAnalysis.py
+++ Project3/FreqAnalysis.py
@@ -7,18 +7,28 @@ input_data = read("bugs_left_turn.wav")
 audio = input_data[1]
 
 '''
-subplot(numrows, numcols, fignum)
-  where fignum ranges from 1 to numrows * numcols;
-  commas optional so subplot(211) is equal to subplot (2, 1, 1)
+SYNTAX FOR subplot()
+	subplot(numrows, numcols, fignum)
+	where fignum ranges from 1 to numrows * numcols;
+	commas optional so subplot(211) is equal to subplot (2, 1, 1)
 '''
 
-# plot the first 1024 sample
-plt.figure(1)					# the first figure
-plt.subplot(3, 1, 1)			# the first subplot in the first figure
-plt.plot(audio[0:1024], 'b:')	# 'b:' indicates blue dotted lines
+''' PLOT THE FIRST 1024 SAMPLES '''
+# the first figure
+plt.figure(1)
 
-plt.subplot(3, 1, 2)			# the second subplot in the first figure
-plt.plot(audio[1025:2048], 'm,')# 'm,' indicates magenta pixels
+# the first subplot in the first figure					
+plt.subplot(3, 1, 1)		
+
+# 'b:' indicates blue dotted lines	
+plt.plot(audio[0:1024], 'b:')	
+
+''' PLOT SAMPLES 1025 TO 2048 '''
+# the second subplot in the first figure
+plt.subplot(3, 1, 2)			
+
+# 'm,' indicates magenta pixels
+plt.plot(audio[1025:2048], 'm,')
 
 # label the axes
 plt.ylabel("Amplitude")
