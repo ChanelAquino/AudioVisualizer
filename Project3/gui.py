9d6bc19cf0941f7a23a53bba52e8d2cb49d471b9
Chanel Aquino <chanelcodes@gmail.com>
2016-04-28 13:38:47 -0700

    more gui

--- Project3/gui.py
+++ Project3/gui.py
@@ -2,14 +2,20 @@ import Tkinter as tk
 from Tkinter import *
 import tkFont
 
+#TODO: change names of functions/methods and variables
+#TODO: change color scheme, font faces, etc.
+#TODO: plot stuff
+
+
 #----------------------------------------#
 #----------FONT CUSTOM INFO--------------#
 #----------------------------------------#
-TITLE_FONT = ("Helvetica", 45, "bold")
-BUTTON_COLOR = "#1A1511"
+TITLE_FONT = ('times', 20, "bold")
+BUTTON_COLOR = "#1A1511" 
 BG_COLOR = "#68625E"
 BUTTON_TEXT_COLOR = "#9D958D"
 BUTTON_HIGHLIGHT = "#D4D2CF"
+
 
 
 
@@ -27,7 +33,7 @@ class SampleApp(tk.Tk):
 		container.grid_columnconfigure(0, weight=1)
 
 		self.frames = {}
-		for F in (StartPage, PageOne, PageTwo):
+		for F in (SongSelection, PageOne, VisualSelection):
 			page_name = F.__name__
 			frame = F(container, self)
 			self.frames[page_name] = frame
@@ -38,7 +44,7 @@ class SampleApp(tk.Tk):
 			frame.grid(row=0, column=0, sticky="nsew")
 			frame.configure(bg=BG_COLOR) 
 
-		self.show_frame("StartPage")
+		self.show_frame("SongSelection")
 
 	def show_frame(self, page_name):
 		'''Show a frame for the given page name'''
@@ -46,22 +52,23 @@ class SampleApp(tk.Tk):
 		frame.tkraise()
 
 
-class StartPage(tk.Frame):
+class SongSelection(tk.Frame):
 
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 		self.controller = controller
-		label = tk.Label(self, text="Choose a song.", bg = BUTTON_TEXT_COLOR, width = "45", height ="5", fg = BUTTON_COLOR, font=TITLE_FONT)
+		label = tk.Label(self, text="Choose a song.", bg = BUTTON_TEXT_COLOR, width = "45", height ="5", fg = BUTTON_COLOR)
+		label.config(font=TITLE_FONT)
 
 		label.pack(side="top", fill="x", pady=10)
 
-		button1 = tk.Button(self, text="HANDS DOWN",  fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR,  width="45", height="10", highlightbackground=BUTTON_COLOR, activebackground= BUTTON_HIGHLIGHT, command=lambda: controller.show_frame("PageOne"))
+		button1 = tk.Button(self, text="HANDS DOWN",  fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR,  width="45", height="10", highlightbackground=BUTTON_COLOR, activebackground= BUTTON_HIGHLIGHT, command=lambda: controller.show_frame("VisualSelection"))
 
 		button2 = tk.Button(self, text="RETURN OF THE MACK", fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR,  width="45", height="10", highlightbackground=BUTTON_COLOR,  activebackground= BUTTON_HIGHLIGHT, 
-				            command=lambda: controller.show_frame("PageTwo"))
+				            command=lambda: controller.show_frame("VisualSelection"))
 
 		button3 = tk.Button(self, text="TBD", fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR, width="45", height="10", highlightbackground=BUTTON_COLOR,  activebackground= BUTTON_HIGHLIGHT, 
-				            command=lambda: controller.show_frame("PageTwo"))
+				            command=lambda: controller.show_frame("VisualSelection"))
 
 
 		button1.pack()
@@ -75,23 +82,42 @@ class PageOne(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 		self.controller = controller
-		label = tk.Label(self, text="This is page 1", font=TITLE_FONT)
+		label = tk.Label(self, text="This is page 1")
+		label.config(font=TITLE_FONT)
 		label.pack(side="top", fill="x", pady=10)
 		button = tk.Button(self, text="Go to the start page",
 				           command=lambda: controller.show_frame("StartPage"))
 		button.pack()
 
 
-class PageTwo(tk.Frame):
+class VisualSelection(tk.Frame):
 
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 		self.controller = controller
-		label = tk.Label(self, text="This is page 2", font=TITLE_FONT)
+		label = tk.Label(self, text="Select a visualizer.", bg = BUTTON_TEXT_COLOR, width = "45", height ="5", fg = BUTTON_COLOR)
+		label.config(font=TITLE_FONT)
+
 		label.pack(side="top", fill="x", pady=10)
-		button = tk.Button(self, text="Go to the start page",
-				           command=lambda: controller.show_frame("StartPage"))
-		button.pack()
+		button1 = tk.Button(self, text="METHOD #1",  fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR,  width="45", height="8", highlightbackground=BUTTON_COLOR, activebackground= BUTTON_HIGHLIGHT, command=lambda: controller.show_frame("VisualSelection"))
+
+		button2 = tk.Button(self, text="METHOD #2", fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR,  width="45", height="8", highlightbackground=BUTTON_COLOR,  activebackground= BUTTON_HIGHLIGHT, 
+				            command=lambda: controller.show_frame("VisualSelection"))
+
+		button3 = tk.Button(self, text="METHOD #3", fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR, width="45", height="8", highlightbackground=BUTTON_COLOR,  activebackground= BUTTON_HIGHLIGHT, 
+				            command=lambda: controller.show_frame("VisualSelection"))
+
+		button4 = tk.Button(self, text="METHOD #4", fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR, width="45", height="8", highlightbackground=BUTTON_COLOR,  activebackground= BUTTON_HIGHLIGHT, 
+				            command=lambda: controller.show_frame("VisualSelection"))
+
+		button5 = tk.Button(self, text="Select a different song.", fg = BUTTON_TEXT_COLOR, bg = BUTTON_COLOR, width="45", height="4", highlightbackground=BUTTON_COLOR,  activebackground= BUTTON_HIGHLIGHT, 
+				            command=lambda: controller.show_frame("SongSelection"))
+
+		button1.pack()
+		button2.pack()
+		button3.pack()
+		button4.pack()
+		button5.pack()
 
 
 
@@ -99,4 +125,5 @@ if __name__ == "__main__":
 	app = SampleApp()
 	app.geometry("800x850")
 	app.mainloop()
+	app.title("Audio Visualizer")
 
