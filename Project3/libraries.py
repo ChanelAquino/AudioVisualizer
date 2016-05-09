b4048fb643ce1e3dc5e36db51772db120e7dd9b1
Chanel Aquino <chanelcodes@gmail.com>
2016-05-09 15:31:41 -0700

    blah

--- Project3/libraries.py
+++ Project3/libraries.py
@@ -1,11 +1,11 @@
 # sound_analysis.py
+import scipy
 from scipy.io.wavfile import read
 from scipy.signal import hann
 from scipy.fftpack import rfft, fft
 import matplotlib as mpl
 mpl.use("TkAgg")
 import matplotlib.pyplot as plt
-import scipy
 import numpy as np
 import wave
 import sys
