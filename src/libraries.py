cd66306652810b4aaee9eb2b68777b4b865daa62
Chanel A <ChanelAquino@users.noreply.github.com>
2016-05-13 18:56:15 -0700

    update libraries to enable bokeh

--- src/libraries.py
+++ src/libraries.py
@@ -1,7 +1,9 @@
-# sound_analysis.py
+# needed for sound_analysis.py
 from scipy.io.wavfile import read
 from scipy.signal import hann
 from scipy.fftpack import rfft, fft
+
+# matplotlib libraries
 import matplotlib as mpl
 mpl.use("TkAgg")
 import matplotlib.pyplot as plt
@@ -9,3 +11,9 @@ import scipy
 import numpy as np
 import wave
 import sys
+
+# bokeh libraries
+from bokeh.plotting import figure, output_file, show
+from bokeh.charts import Scatter, output_file, show
+from bokeh.io import vform
+from bokeh.models import CustomJS, ColumnDataSource, Slider
